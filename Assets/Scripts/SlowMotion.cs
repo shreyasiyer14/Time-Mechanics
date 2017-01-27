8f26d2f9c77403c6c3ce806457004de9c9df24d4
Shreyas Iyer <shreyas@shitjustgotreal.in>
2017-01-27 17:45:45 +0530

    Initial commit. Slowing down time with example scene

---
+++ Assets/Scripts/SlowMotion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowMotion : MonoBehaviour {
+	[SerializeField]
+	private float slowDownFactor;
+
+	[SerializeField]
+	private float duration;
+
+	void Update () {
+		Time.timeScale += (1f / duration) * Time.unscaledDeltaTime;
+		Time.timeScale = Mathf.Clamp (Time.timeScale, 0f, 1f);
+		Time.fixedDeltaTime = 0.02f * Time.timeScale;
+	}
+
+	public void SlowMotionFunc() {
+		Time.timeScale = slowDownFactor;
+		Time.fixedDeltaTime = 0.02f * Time.timeScale;
+	}
+}
---
+++ Assets/Scripts/TriggerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour {
+
+	private Camera cam;
+	[SerializeField]
+	private SlowMotion timeManager;
+
+	[SerializeField]
+	private GameObject explosion;
+
+	void Start() {
+		cam = Camera.main;
+	}
+	void Update () {
+		RaycastHit _hit;
+		if (Input.GetButtonDown ("Fire1")) {
+			if (Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit)) {	
+				Instantiate (explosion, _hit.point, Quaternion.LookRotation(_hit.normal));
+				timeManager.SlowMotionFunc ();
+			}
+		}
+	}
+}
