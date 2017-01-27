ddf795712e62a05aeeb502046346f2011f62f904
Shreyas Iyer <shreyasiyer14@gmail.com>
2017-01-27 18:41:39 +0530

    Included the base for reversing the time. WIP

---
+++ Assets/Scripts/TimeReverse.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeReverse : MonoBehaviour {
+
+	public GameObject testObject;
+
+	[SerializeField]
+	private float duration;
+
+	private bool reverseTime = false;
+	private LinkedList<Vector3> positions;
+	private LinkedList<Quaternion> rotations;
+
+	void Start () {
+		//InvokeRepeating ("ClearArray", 1f, 5f);
+	}
+
+	void Update () {
+		if (!reverseTime) {
+			positions.AddFirst (testObject.transform.position);
+			rotations.AddFirst (testObject.transform.rotation);
+			Debug.Log (positions.Count);
+		}
+		if (reverseTime) {
+			testObject.GetComponent<Rigidbody> ().useGravity = false;
+			testObject.transform.position = positions.First.Value;
+			testObject.transform.rotation = rotations.First.Value;
+			positions.RemoveFirst ();
+			rotations.RemoveFirst ();
+		}
+	}
+
+	public void TimeReverseFunc() {
+		reverseTime = true;
+	}
+
+	void ClearArray() {
+		positions.Clear ();
+		rotations.Clear ();
+	}
+}
---
+++ Assets/Scripts/TriggerScript2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript2 : MonoBehaviour {
+	private Camera cam;
+	[SerializeField]
+	private TimeReverse timeManager;
+
+	[SerializeField]
+	private GameObject explosion;
+
+	void Start() {
+		cam = Camera.main;
+	}
+	void Update () {
+		RaycastHit _hit;
+		if (Input.GetButtonDown ("Fire1")) {
+			if (Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit)) {	
+				Instantiate (explosion, _hit.point, Quaternion.LookRotation(_hit.normal));
+				timeManager.TimeReverseFunc ();
+			}
+		}
+	}
+}
